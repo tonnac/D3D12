0bdf4e3526ab8566bf656c503bcb52cf92587b17
tonnac <tonnac35@gmail.com>
2018-10-25 02:38:20 +0900

    Device Create

---
+++ Source/Init_Direct3D/InitDirect3DApp.cpp
@@ -0,0 +1,92 @@
+#include "d3dApp.h"
+#include <DirectXColors.h>
+
+using namespace DirectX;
+
+class InitDirect3DApp : public D3DApp
+{
+public:
+	InitDirect3DApp(HINSTANCE hInstance);
+	~InitDirect3DApp();
+
+	virtual bool initialize() override;
+
+private:
+	virtual void OnResize() override;
+	virtual void Update(const GameTimer& get)override;
+	virtual void Draw(const GameTimer& get)override;
+};
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, PSTR cmdLine, int showCmd)
+{
+#if defined(DEBUG) || defined(_DEBUG)
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+	try
+	{
+		InitDirect3DApp theApp(hInstance);
+		if (theApp.initialize() == false)
+		{
+			return 0;
+		}
+		return theApp.Run();
+	}
+	catch (DxException& e)
+	{
+		MessageBox(nullptr, e.ToString().c_str(), L"HR Failed", MB_OK);
+		return 0;
+	}
+}
+
+
+InitDirect3DApp::InitDirect3DApp(HINSTANCE hInstance) : D3DApp(hInstance)
+{
+}
+InitDirect3DApp::~InitDirect3DApp() 
+{
+}
+
+bool InitDirect3DApp::initialize()
+{
+	if (D3DApp::initialize() == false)
+		return false;
+	return true;
+}
+
+void InitDirect3DApp::OnResize()
+{
+	D3DApp::OnResize();
+}
+void InitDirect3DApp::Update(const GameTimer& get)
+{
+
+}
+void InitDirect3DApp::Draw(const GameTimer& get)
+{
+	ThrowIfFailed(mDirectCmdListAlloc->Reset());
+
+	ThrowIfFailed(mCommandList->Reset(mDirectCmdListAlloc.Get(), nullptr));
+
+	mCommandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(CurrentBackBuffer(), D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET));
+
+	mCommandList->RSSetViewports(1, &mScreenViewPort);
+	mCommandList->RSSetScissorRects(1, &mScissorRect);
+
+	mCommandList->ClearRenderTargetView(CurrentBackBufferView(), Colors::LightSteelBlue, 0, nullptr);
+	mCommandList->ClearDepthStencilView(DepthStencilView(), D3D12_CLEAR_FLAG_DEPTH | D3D12_CLEAR_FLAG_STENCIL, 1.0f, 0, 0, nullptr);
+
+	mCommandList->OMSetRenderTargets(1, &CurrentBackBufferView(), true, &DepthStencilView());
+
+	mCommandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(CurrentBackBuffer(), D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT));
+
+	ThrowIfFailed(mCommandList->Close());
+
+	ID3D12CommandList* cmdsLists[] = { mCommandList.Get() };
+	mCommandQueue->ExecuteCommandLists(_countof(cmdsLists), cmdsLists);
+
+	ThrowIfFailed(mSwapChain->Present(0, 0));
+	mCurrBackBuffer = (mCurrBackBuffer + 1) % SwapChainBufferCount;
+
+	FlushCommandQueue();
+}
