c7c61f6fcb55ab4ffc1e5fad647bbedf67d2c1fe
tonnac <tonnac35@gmail.com>
2018-11-16 03:56:03 +0900

    crate

--- Common/d3dUtil.h
+++ Common/d3dUtil.h
@@ -22,6 +22,7 @@
 #include <cstdlib>
 #include "d3dx12.h"
 #include "MathHelper.h"
+#include "DDSTextureLoader.h"
 
 extern const int gNumFrameResources;
 
@@ -162,6 +163,16 @@ struct Material
 	DirectX::XMFLOAT3 FresnelR0 = { 0.01f, 0.01f, 0.01f };
 	float Roughness = 0.25f;
 	DirectX::XMFLOAT4X4 MatTransform = MathHelper::Identity4x4();
+};
+
+struct Texture
+{
+	std::string Name;
+
+	std::wstring Filename;
+
+	Microsoft::WRL::ComPtr<ID3D12Resource> Resource = nullptr;
+	Microsoft::WRL::ComPtr<ID3D12Resource> UploadHeap = nullptr;
 };
 
 
