e30612e5386411de745436ffb977296b678a5e47
tonnac <tonnac35@gmail.com>
2018-12-08 04:08:27 +0900

    cubemap debugging

---
+++ Source/CubeMap/common.hlsl
@@ -0,0 +1,72 @@
+#ifndef NUM_DIR_LIGHTS
+#define NUM_DIR_LIGHTS 3
+#endif
+
+#ifndef NUM_POINT_LIGHTS
+#define NUM_POINT_LIGHTS 0
+#endif
+
+#ifndef NUM_SPOT_LIGHTS
+#define NUM_SPOT_LIGHTS 0
+#endif
+
+#include "LightingUtil.hlsl"
+
+struct MaterialData
+{
+	float4   DiffuseAlbedo;
+	float3   FresnelR0;
+	float    Roughness;
+	float4x4 MatTransform;
+	uint     DiffuseMapIndex;
+	uint     MatPad0;
+	uint     MatPad1;
+	uint     MatPad2;
+};
+
+TextureCube gCubeMap : register(t0);
+
+Texture2D gDiffuseMap[4] : register(t1);
+
+StructuredBuffer<MaterialData> gMaterialData : register(t0, space1);
+
+
+SamplerState gsamPointWrap        : register(s0);
+SamplerState gsamPointClamp       : register(s1);
+SamplerState gsamLinearWrap       : register(s2);
+SamplerState gsamLinearClamp      : register(s3);
+SamplerState gsamAnisotropicWrap  : register(s4);
+SamplerState gsamAnisotropicClamp : register(s5);
+
+cbuffer cbPerObject : register(b0)
+{
+	float4x4 gWorld;
+	float4x4 gTexTransform;
+	uint gMaterialIndex;
+	uint gObjPad0;
+	uint gObjPad1;
+	uint gObjPad2;
+};
+
+cbuffer cbPass : register(b1)
+{
+	float4x4 gView;
+	float4x4 gInvView;
+	float4x4 gProj;
+	float4x4 gInvProj;
+	float4x4 gViewProj;
+	float4x4 gInvViewProj;
+	float3 gEyePosW;
+	float cbPerObjectPad1;
+	float2 gRenderTargetSize;
+	float2 gInvRenderTargetSize;
+	float gNearZ;
+	float gFarZ;
+	float gTotalTime;
+	float gDeltaTime;
+	float4 gAmbientLight;
+
+	Light gLights[MaxLights];
+};
+
+
